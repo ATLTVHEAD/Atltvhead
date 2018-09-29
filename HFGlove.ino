83b24c4c04ec4ac30157eab3a09bd580a199f1da
ndamen3 <ndamen3@users.noreply.github.com>
2018-09-28 23:50:33 -0400

    Update HFGlove.ino

--- HFGlove.ino
+++ HFGlove.ino
@@ -9,20 +9,20 @@ Written Nate Damen
 #include <IRCClient.h>
 #include <Math.h>
 /*-----------------------------------------------------------------------------------*/
-#define ssid1         "IgotthePoops"
-#define password1     "AcerbiNimbus"
+#define ssid1         
+#define password1     
 
-#define ssid2         "DestructionDynamics"
-#define password2     "CealiNimbus"
+#define ssid2         
+#define password2     
 
-#define ssid3          "ATT8Jmy7qw"
-#define password3      "7pbw6k?shf9p"
+#define ssid3          
+#define password3      
 
 #define IRC_SERVER   "irc.chat.twitch.tv"
 #define IRC_PORT     6667
-#define IRC_NICK     "tvheadbot"
-#define IRC_CHAN     "#atltvhead"
-#define IRC_PASS     "oauth:0hr40w69fcqdgl8frc4ti09i5g3s3w"
+#define IRC_NICK     
+#define IRC_CHAN     
+#define IRC_PASS     
 
 //naming wifi and server client
 
